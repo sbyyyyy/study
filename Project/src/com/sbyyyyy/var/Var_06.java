541ede7d4fa4cff74f82206bd15739606d7176c9
sbyyyyy <affordu@gmail.com>
2022-03-25 09:31:40 +0900

    added com.study

--- Project/src/com/sbyyyyy/var/Var_06.java
+++ Project/src/com/sbyyyyy/var/Var_06.java
@@ -4,21 +4,23 @@ public class Var_06 {
 	public static void main(String[] args) {
 		int intNum1 = 552;
 		int intNum2 = 33;
-		
+
 		int intSum;
-		int intDefference;
+		int intDifference;
 		int intProduct;
-		int intDivdie;
+		int intDivide;
 		int intMod;
-		
+
 		intSum = intNum1 + intNum2;
-		intDefference = intNum1 - intNum2;
+		intDifference = intNum1 - intNum2;
 		intProduct = intNum1 * intNum2;
-		intDivdie = intNum1 / intNum2;
+		intDivide = intNum1 / intNum2;
 		intMod = intNum1 % intNum2;
+
+		System.out.println(intSum);
+		System.out.println(intDifference);
+		System.out.println(intProduct);
+		System.out.println(intDivide);
 		System.out.println(intMod);
-		
-		
-		
 	}
 }
